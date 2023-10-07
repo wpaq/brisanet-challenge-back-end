4a0dd1caf0d3d7349c5cedee572ef3c0b494080f
wpaq <wallysonpabloo@gmail.com>
2023-10-07 17:45:32 -0300

    test: ensure ProfessorController returns 400 if no nome is provided

---
+++ tests/presentation/controllers/professor-controller.spec.ts
@@ -0,0 +1,17 @@
+import { ProfessorController } from '@/presentation/index'
+
+describe('Professor Controller', () => {
+  test('Should return 400 if no /nome/ is provided', async () => {
+    const sut = new ProfessorController()
+    const httpRequest = {
+      body: {
+        // nome: 'any_nome',
+        telefone: 123456789,
+        email: 'any_email@mail.com',
+        cpf: 12345678910
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
