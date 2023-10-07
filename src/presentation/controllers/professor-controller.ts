de0dcfa0380f4919f1760dd80e2bea9e88c9d05d
wpaq <wallysonpabloo@gmail.com>
2023-10-07 18:39:59 -0300

    feat: ensure ProfessorController returns 500 if EmailValidator throws

--- src/presentation/controllers/professor-controller.ts
+++ src/presentation/controllers/professor-controller.ts
@@ -1,27 +1,34 @@
 import { type EmailValidator, type Controller, type HttpRequest, type HttpResponse } from '@/presentation/protocols'
-import { InvalidParamError, MissingParamError } from '@/presentation/errors'
+import { InvalidParamError, MissingParamError, ServerError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/helpers'
 
 export class ProfessorController implements Controller {
   constructor (private readonly emailValidator: EmailValidator) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['nome', 'telefone', 'email', 'cpf']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['nome', 'telefone', 'email', 'cpf']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
 
-    const { email } = httpRequest.body
-    const isValid = this.emailValidator.isValid(email)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
+      const { email } = httpRequest.body
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
 
-    return {
-      statusCode: 200,
-      body: 'ok'
+      return {
+        statusCode: 200,
+        body: 'ok'
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
