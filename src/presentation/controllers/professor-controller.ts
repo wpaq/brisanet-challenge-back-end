e67fd9764ddfec5cbd80237e4835266152d0336e
wpaq <wallysonpabloo@gmail.com>
2023-10-07 18:04:01 -0300

    feat: ensure ProfessorController returns 400 if no telefone is provided

--- src/presentation/controllers/professor-controller.ts
+++ src/presentation/controllers/professor-controller.ts
@@ -3,9 +3,22 @@ import { MissingParamError } from '@/presentation/errors'
 
 export class ProfessorController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const { nome, telefone } = httpRequest.body
+    if (!nome) {
+      return {
+        statusCode: 400,
+        body: new MissingParamError('nome')
+      }
+    }
+    if (!telefone) {
+      return {
+        statusCode: 400,
+        body: new MissingParamError('telefone')
+      }
+    }
     return {
-      statusCode: 400,
-      body: new MissingParamError('nome')
+      statusCode: 200,
+      body: 'ok'
     }
   }
 }
