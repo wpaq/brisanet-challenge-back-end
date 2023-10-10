e3e0047df85107bcee0a0e41a39d2875cf4886c4
wpaq <wallysonpabloo@gmail.com>
2023-10-10 09:35:14 -0300

    refactor: remove unused code

--- tests/infra/db/prisma/professor-prisma-repository.spec.ts
+++ tests/infra/db/prisma/professor-prisma-repository.spec.ts
@@ -1,32 +1,21 @@
-import { createProfessor } from './function-with-context'
-import { type MockContext, type Context, createMockContext } from '@/tests/infra/db/prisma/context'
-import { mockAddProfessorParams, mockProfessorModel } from '@/tests/domain/mock-professor'
+import { mockAddProfessorParams } from '@/tests/domain/mock-professor'
 import { PrismaHelper, ProfessorPrismaRepository } from '@/infra/db/prisma'
 
 describe('ProfessorPrismaRepository', () => {
-  let mockCtx: MockContext
-  let ctx: Context
-
   beforeEach(async () => {
-    mockCtx = createMockContext()
-    ctx = mockCtx as unknown as Context
     await PrismaHelper.deleteMany()
   })
 
   test('Should return an professor on success', async () => {
     const sut = new ProfessorPrismaRepository()
     const addProfessorParams = mockAddProfessorParams()
-    const addProfessorModel = mockProfessorModel()
     const result = await sut.add(addProfessorParams)
 
-    mockCtx.prisma.professor.create.mockResolvedValue(addProfessorModel)
-
-    await expect(createProfessor(result, ctx)).resolves.toEqual({
-      id: 'valid_id',
-      nome: 'valid_nome',
-      telefone: '123456789',
-      email: 'valid_email@mail.com',
-      cpf: '12345678910'
-    })
+    expect(result).toBeTruthy()
+    expect(result.id).toBeTruthy()
+    expect(result.nome).toBe('valid_nome')
+    expect(result.telefone).toBe('123456789')
+    expect(result.email).toBe('valid_email@mail.com')
+    expect(result.cpf).toBe('12345678910')
   })
 })
