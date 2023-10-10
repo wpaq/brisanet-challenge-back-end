49088da9026655f71a667b1c76735e45b7b756e7
wpaq <wallysonpabloo@gmail.com>
2023-10-10 11:47:02 -0300

    feat: ensure EmailValidation return an error if EmailValidator returns false

---
+++ src/validation/validators/email-validation.ts
@@ -0,0 +1,18 @@
+import { type EmailValidator } from '@/validation/protocols/email-validator'
+import { type Validation } from '@/presentation/protocols'
+import { InvalidParamError } from '@/presentation/errors'
+
+export class EmailValidation implements Validation {
+  constructor (
+    private readonly fieldName: string,
+    private readonly emailValidator: EmailValidator
+  ) {}
+
+  validate (input: any): Error | null {
+    const isValid = this.emailValidator.isValid(input[this.fieldName])
+    if (!isValid) {
+      return new InvalidParamError(this.fieldName)
+    }
+    return null
+  }
+}
---
+++ tests/validation/mocks/mock-email-validation.ts
@@ -0,0 +1,11 @@
+import { type EmailValidator } from '@/validation/protocols'
+
+export class EmailValidatorSpy implements EmailValidator {
+  isEmailValid = true
+  email: string
+
+  isValid (email: string): boolean {
+    this.email = email
+    return this.isEmailValid
+  }
+}
