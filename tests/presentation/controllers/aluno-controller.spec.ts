1f792d236594b01ac291889d20b8b289aaf14416
wpaq <wallysonpabloo@gmail.com>
2023-10-14 12:40:39 -0300

    test: ensure AlunoController returns 400 if an required field is no provided

---
+++ tests/presentation/controllers/aluno-controller.spec.ts
@@ -0,0 +1,98 @@
+import { AlunoController } from '@/presentation/controllers'
+import { AddAlunoSpy, EmailValidatorSpy } from '@/tests/presentation/mocks'
+import { badRequest } from '@/presentation/helpers'
+import { MissingParamError } from '@/presentation/errors'
+
+type SutTypes = {
+  sut: AlunoController
+  emailValidatorSpy: EmailValidatorSpy
+  addAlunoSpy: AddAlunoSpy
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorSpy = new EmailValidatorSpy()
+  const addAlunoSpy = new AddAlunoSpy()
+  const sut = new AlunoController(emailValidatorSpy, addAlunoSpy)
+  return {
+    sut,
+    emailValidatorSpy,
+    addAlunoSpy
+  }
+}
+
+describe('Aluno Controller', () => {
+  test('Should return 400 if no /nome/ is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        // nome: 'any_nome',
+        telefone: 123456789,
+        email: 'any_email@mail.com',
+        cpf: 12345678910,
+        matricula: 'any_matricula'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('nome')))
+  })
+
+  test('Should return 400 if no /telefone/ is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        nome: 'any_nome',
+        // telefone: 123456789,
+        email: 'any_email@mail.com',
+        cpf: 12345678910,
+        matricula: 'any_matricula'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('telefone')))
+  })
+
+  test('Should return 400 if no /email/ is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        nome: 'any_nome',
+        telefone: 123456789,
+        // email: 'any_email@mail.com',
+        cpf: 12345678910,
+        matricula: 'any_matricula'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+
+  test('Should return 400 if no /cpf/ is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        nome: 'any_nome',
+        telefone: 123456789,
+        email: 'any_email@mail.com',
+        // cpf: 12345678910
+        matricula: 'any_matricula'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('cpf')))
+  })
+
+  test('Should return 400 if no /matricula/ is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        nome: 'any_nome',
+        telefone: 123456789,
+        email: 'any_email@mail.com',
+        cpf: 12345678910
+        // matricula: 'any_matricula'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('matricula')))
+  })
+})
