104d596b6b3632336d1933ebf8ca68e307138363
wpaq <wallysonpabloo@gmail.com>
2023-10-14 10:56:54 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository with correct error if controller returns a server error

--- tests/main/decorators/log-controller-decorator.spec.ts
+++ tests/main/decorators/log-controller-decorator.spec.ts
@@ -1,6 +1,7 @@
 import { LogControllerDecorator } from '@/main/decorators'
-import { ok } from '@/presentation/helpers'
+import { ok, serverError } from '@/presentation/helpers'
 import { type Controller, type HttpRequest, type HttpResponse } from '@/presentation/protocols'
+import { LogErrorRepositorySpy } from '@/tests/data/mocks/mock-db-log'
 
 const mockFakeRequest = (): HttpRequest => ({
   body: {
@@ -10,6 +11,12 @@ const mockFakeRequest = (): HttpRequest => ({
     cpf: '12345678910'
   }
 })
+
+const mockServerError = (): HttpResponse => {
+  const fakeError = new Error()
+  fakeError.stack = 'any_stack'
+  return serverError(fakeError)
+}
 
 class ControllerSpy implements Controller {
   httpResponse = ok('any_data')
@@ -24,14 +31,17 @@ class ControllerSpy implements Controller {
 interface SutTypes {
   sut: LogControllerDecorator
   controllerSpy: ControllerSpy
+  logErrorRepositorySpy: LogErrorRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
   const controllerSpy = new ControllerSpy()
-  const sut = new LogControllerDecorator(controllerSpy)
+  const logErrorRepositorySpy = new LogErrorRepositorySpy()
+  const sut = new LogControllerDecorator(controllerSpy, logErrorRepositorySpy)
   return {
     sut,
-    controllerSpy
+    controllerSpy,
+    logErrorRepositorySpy
   }
 }
 
@@ -49,5 +59,13 @@ describe('LogController Decorator', () => {
     const httpRequest = mockFakeRequest()
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(controllerSpy.httpResponse)
+  })
+
+  test('Should call LogErrorRepository with correct error if controller returns a server error', async () => {
+    const { sut, controllerSpy, logErrorRepositorySpy } = makeSut()
+    const serverError = mockServerError()
+    controllerSpy.httpResponse = serverError
+    await sut.handle(mockFakeRequest())
+    expect(logErrorRepositorySpy.stack).toBe(serverError.body.stack)
   })
 })
