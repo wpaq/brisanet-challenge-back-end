0ad5fc8e6e31f66c0cbe36e2494ccb39d47e8764
wpaq <wallysonpabloo@gmail.com>
2023-10-15 12:39:05 -0300

    refactor: remove unused tests

--- tests/presentation/controllers/aluno-controller.spec.ts
+++ tests/presentation/controllers/aluno-controller.spec.ts
@@ -1,149 +1,53 @@
+import { AddAlunoSpy, ValidationSpy } from '@/tests/presentation/mocks'
 import { AlunoController } from '@/presentation/controllers'
-import { AddAlunoSpy, EmailValidatorSpy } from '@/tests/presentation/mocks'
 import { badRequest, serverError } from '@/presentation/helpers'
-import { InvalidParamError, MissingParamError } from '@/presentation/errors'
+import { MissingParamError } from '@/presentation/errors'
 
 type SutTypes = {
   sut: AlunoController
-  emailValidatorSpy: EmailValidatorSpy
   addAlunoSpy: AddAlunoSpy
+  validationSpy: ValidationSpy
 }
 
 const makeSut = (): SutTypes => {
-  const emailValidatorSpy = new EmailValidatorSpy()
   const addAlunoSpy = new AddAlunoSpy()
-  const sut = new AlunoController(emailValidatorSpy, addAlunoSpy)
+  const validationSpy = new ValidationSpy()
+  const sut = new AlunoController(addAlunoSpy, validationSpy)
   return {
     sut,
-    emailValidatorSpy,
-    addAlunoSpy
+    addAlunoSpy,
+    validationSpy
   }
 }
 
 describe('Aluno Controller', () => {
-  test('Should return 400 if no /nome/ is provided', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        // nome: 'any_nome',
-        telefone: 123456789,
-        email: 'any_email@mail.com',
-        cpf: 12345678910,
-        matricula: 'any_matricula'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('nome')))
-  })
-
-  test('Should return 400 if no /telefone/ is provided', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        nome: 'any_nome',
-        // telefone: 123456789,
-        email: 'any_email@mail.com',
-        cpf: 12345678910,
-        matricula: 'any_matricula'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('telefone')))
-  })
-
-  test('Should return 400 if no /email/ is provided', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        nome: 'any_nome',
-        telefone: 123456789,
-        // email: 'any_email@mail.com',
-        cpf: 12345678910,
-        matricula: 'any_matricula'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
-  })
-
-  test('Should return 400 if no /cpf/ is provided', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        nome: 'any_nome',
-        telefone: 123456789,
-        email: 'any_email@mail.com',
-        // cpf: 12345678910
-        matricula: 'any_matricula'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('cpf')))
-  })
-
-  test('Should return 400 if no /matricula/ is provided', async () => {
-    const { sut } = makeSut()
+  test('Should call Validation with correct value', async () => {
+    const { sut, validationSpy } = makeSut()
     const httpRequest = {
       body: {
         nome: 'any_nome',
         telefone: 123456789,
         email: 'any_email@mail.com',
         cpf: 12345678910
-        // matricula: 'any_matricula'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('matricula')))
-  })
-
-  test('Should return 400 if an invalid /email/ is provided', async () => {
-    const { sut, emailValidatorSpy } = makeSut()
-    jest.spyOn(emailValidatorSpy, 'isValid').mockReturnValueOnce(false)
-    const httpRequest = {
-      body: {
-        nome: 'any_nome',
-        telefone: 123456789,
-        email: 'invalid_email@mail.com',
-        cpf: 12345678910,
-        matricula: 'any_matricula'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
-  })
-
-  test('Should call EmailValidator with correct email', async () => {
-    const { sut, emailValidatorSpy } = makeSut()
-    const httpRequest = {
-      body: {
-        nome: 'any_nome',
-        telefone: 123456789,
-        email: 'any_email@mail.com',
-        cpf: 12345678910,
-        matricula: 'any_matricula'
       }
     }
     await sut.handle(httpRequest)
-    expect(emailValidatorSpy.email).toBe('any_email@mail.com')
+    expect(validationSpy.input).toEqual(httpRequest.body)
   })
 
-  test('Should return 500 if EmailValidator throws', async () => {
-    const { sut, emailValidatorSpy } = makeSut()
-    jest.spyOn(emailValidatorSpy, 'isValid').mockImplementationOnce(() => {
-      throw new Error()
-    })
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.error = new MissingParamError('any_field')
     const httpRequest = {
       body: {
         nome: 'any_nome',
         telefone: 123456789,
         email: 'any_email@mail.com',
-        cpf: 12345678910,
-        matricula: 'any_matricula'
+        cpf: 12345678910
       }
     }
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse).toEqual(serverError(new Error()))
+    expect(httpResponse).toEqual(badRequest(validationSpy.error))
   })
 
   test('Should call AddAluno with correct values', async () => {
--- tests/presentation/controllers/professor-controller.spec.ts
+++ tests/presentation/controllers/professor-controller.spec.ts
@@ -1,23 +1,20 @@
 import { ProfessorController } from '@/presentation/controllers'
-import { MissingParamError, InvalidParamError } from '@/presentation/errors'
+import { MissingParamError } from '@/presentation/errors'
 import { badRequest, serverError } from '@/presentation/helpers'
-import { AddProfessorSpy, EmailValidatorSpy, ValidationSpy } from '../mocks'
+import { AddProfessorSpy, ValidationSpy } from '../mocks'
 
 type SutTypes = {
   sut: ProfessorController
-  emailValidatorSpy: EmailValidatorSpy
   addProfessorSpy: AddProfessorSpy
   validationSpy: ValidationSpy
 }
 
 const makeSut = (): SutTypes => {
-  const emailValidatorSpy = new EmailValidatorSpy()
   const addProfessorSpy = new AddProfessorSpy()
   const validationSpy = new ValidationSpy()
-  const sut = new ProfessorController(emailValidatorSpy, addProfessorSpy, validationSpy)
+  const sut = new ProfessorController(addProfessorSpy, validationSpy)
   return {
     sut,
-    emailValidatorSpy,
     addProfessorSpy,
     validationSpy
   }
@@ -51,53 +48,6 @@ describe('Professor Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(validationSpy.error))
-  })
-
-  test('Should return 400 if an invalid /email/ is provided', async () => {
-    const { sut, emailValidatorSpy } = makeSut()
-    jest.spyOn(emailValidatorSpy, 'isValid').mockReturnValueOnce(false)
-    const httpRequest = {
-      body: {
-        nome: 'any_nome',
-        telefone: 123456789,
-        email: 'invalid_email@mail.com',
-        cpf: 12345678910
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
-  })
-
-  test('Should call EmailValidator with correct email', async () => {
-    const { sut, emailValidatorSpy } = makeSut()
-    const httpRequest = {
-      body: {
-        nome: 'any_nome',
-        telefone: 123456789,
-        email: 'any_email@mail.com',
-        cpf: 12345678910
-      }
-    }
-    await sut.handle(httpRequest)
-    expect(emailValidatorSpy.email).toBe('any_email@mail.com')
-  })
-
-  test('Should return 500 if EmailValidator throws', async () => {
-    const { sut, emailValidatorSpy } = makeSut()
-    jest.spyOn(emailValidatorSpy, 'isValid').mockImplementationOnce(() => {
-      throw new Error()
-    })
-    const httpRequest = {
-      body: {
-        nome: 'any_nome',
-        telefone: 123456789,
-        email: 'any_email@mail.com',
-        cpf: 12345678910
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse).toEqual(serverError(new Error()))
   })
 
   test('Should call AddProfessor with correct values', async () => {
