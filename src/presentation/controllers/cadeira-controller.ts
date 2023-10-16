4ebe111d0e511279ce4cedd46e796f9ce9953b32
wpaq <wallysonpabloo@gmail.com>
2023-10-16 17:35:11 -0300

    feat: ensure CadeiraController returns 500 if AddCadeira throws

--- src/presentation/controllers/cadeira-controller.ts
+++ src/presentation/controllers/cadeira-controller.ts
@@ -1,6 +1,6 @@
 import { type Validation, type Controller, type HttpRequest, type HttpResponse } from '@/presentation/protocols'
 import { type AddCadeira } from '@/domain/usecases/add-cadeira'
-import { badRequest, ok } from '@/presentation/helpers'
+import { badRequest, ok, serverError } from '@/presentation/helpers'
 
 export class CadeiraController implements Controller {
   constructor (
@@ -9,21 +9,25 @@ export class CadeiraController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+
+      const { nome, slug, dataInicio, dataFim, cargaHoraria, professorId } = httpRequest.body
+      const cadeira = await this.addCadeira.add({
+        nome,
+        slug,
+        dataInicio,
+        dataFim,
+        cargaHoraria,
+        professorId
+      })
+
+      return ok(cadeira)
+    } catch (error) {
+      return serverError(error)
     }
-
-    const { nome, slug, dataInicio, dataFim, cargaHoraria, professorId } = httpRequest.body
-    const cadeira = await this.addCadeira.add({
-      nome,
-      slug,
-      dataInicio,
-      dataFim,
-      cargaHoraria,
-      professorId
-    })
-
-    return ok(cadeira)
   }
 }
