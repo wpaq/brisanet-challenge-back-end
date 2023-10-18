d9dd008942226ffae348ec152816b7d175724a9a
wpaq <wallysonpabloo@gmail.com>
2023-10-18 18:08:23 -0300

    feat: ensure CadeiraRoutes return an cadeira on success

--- src/main/factories/usecases/add-cadeira-factory.ts
+++ src/main/factories/usecases/add-cadeira-factory.ts
@@ -1,8 +1,9 @@
 import { DbAddCadeira } from '@/data/usecases'
 import { type AddCadeira } from '@/domain/usecases/add-cadeira'
 import { CadeiraPrismaRepository } from '@/infra/db/prisma'
+import { makeDbCheckProfessorById } from './check-professor-by-id-factory'
 
 export const makeDbAddCadeira = (): AddCadeira => {
   const cadeiraPrismaRepository = new CadeiraPrismaRepository()
-  return new DbAddCadeira(cadeiraPrismaRepository)
+  return new DbAddCadeira(cadeiraPrismaRepository, makeDbCheckProfessorById())
 }
