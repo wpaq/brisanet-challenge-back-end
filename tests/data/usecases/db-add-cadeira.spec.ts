44b5e7ee12a15070113733d58db1bac25dea852d
wpaq <wallysonpabloo@gmail.com>
2023-10-18 17:44:41 -0300

    test: DbAddCadeira returns false if CheckProfessorByIdRepository returns false

--- tests/data/usecases/db-add-cadeira.spec.ts
+++ tests/data/usecases/db-add-cadeira.spec.ts
@@ -1,18 +1,22 @@
-import { mockAddCadeiraParams } from '@/tests/domain/mock-cadeira'
-import { AddCadeiraRepositorySpy } from '@/tests/data/mocks/mock-db-cadeira'
 import { DbAddCadeira } from '@/data/usecases'
+import { CheckProfessorByIdRepositorySpy } from '@/tests/data/mocks/mock-db-professor'
+import { AddCadeiraRepositorySpy } from '@/tests/data/mocks/mock-db-cadeira'
+import { mockAddCadeiraParams } from '@/tests/domain/mock-cadeira'
 
 type SutTypes = {
   sut: DbAddCadeira
   addCadeiraRepositorySpy: AddCadeiraRepositorySpy
+  checkProfessorByIdRepositorySpy: CheckProfessorByIdRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
   const addCadeiraRepositorySpy = new AddCadeiraRepositorySpy()
-  const sut = new DbAddCadeira(addCadeiraRepositorySpy)
+  const checkProfessorByIdRepositorySpy = new CheckProfessorByIdRepositorySpy()
+  const sut = new DbAddCadeira(addCadeiraRepositorySpy, checkProfessorByIdRepositorySpy)
   return {
     sut,
-    addCadeiraRepositorySpy
+    addCadeiraRepositorySpy,
+    checkProfessorByIdRepositorySpy
   }
 }
 
@@ -43,5 +47,12 @@ describe('DbAddCadeira Usecase', () => {
     const { sut, addCadeiraRepositorySpy } = makeSut()
     const result = await sut.add(mockAddCadeiraParams())
     expect(result).toEqual(addCadeiraRepositorySpy.result)
+  })
+
+  test('Should return false if CheckProfessorByIdRepository returns false', async () => {
+    const { sut, checkProfessorByIdRepositorySpy } = makeSut()
+    checkProfessorByIdRepositorySpy.result = false
+    const isValid = await sut.add(mockAddCadeiraParams())
+    expect(isValid).toBe(false)
   })
 })
