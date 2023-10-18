9a32313a282f8f3e4c0d37a17c3123bcbbf5f623
wpaq <wallysonpabloo@gmail.com>
2023-10-18 16:25:03 -0300

    test: ensure DbAddAluno Usecase returns false if CheckAlunoByEmailRepository returns true

--- tests/data/usecases/db-add-aluno.spec.ts
+++ tests/data/usecases/db-add-aluno.spec.ts
@@ -1,18 +1,21 @@
-import { DbAddAluno } from '@/data/usecases'
-import { AddAlunoRepositorySpy } from '@/tests/data/mocks/mock-db-aluno'
 import { mockAddAlunoParams } from '@/tests/domain/mock-aluno'
+import { AddAlunoRepositorySpy, CheckAlunoByEmailRepositorySpy } from '@/tests/data/mocks/mock-db-aluno'
+import { DbAddAluno } from '@/data/usecases'
 
 type SutTypes = {
   sut: DbAddAluno
   addAlunoRepositorySpy: AddAlunoRepositorySpy
+  checkAlunoByEmailRepositorySpy: CheckAlunoByEmailRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
   const addAlunoRepositorySpy = new AddAlunoRepositorySpy()
-  const sut = new DbAddAluno(addAlunoRepositorySpy)
+  const checkAlunoByEmailRepositorySpy = new CheckAlunoByEmailRepositorySpy()
+  const sut = new DbAddAluno(addAlunoRepositorySpy, checkAlunoByEmailRepositorySpy)
   return {
     sut,
-    addAlunoRepositorySpy
+    addAlunoRepositorySpy,
+    checkAlunoByEmailRepositorySpy
   }
 }
 
@@ -37,6 +40,13 @@ describe('DbAddAluno Usecase', () => {
     const addAlunoParams = mockAddAlunoParams()
     const promise = sut.add(addAlunoParams)
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return false if CheckAlunoByEmailRepository returns true', async () => {
+    const { sut, checkAlunoByEmailRepositorySpy } = makeSut()
+    checkAlunoByEmailRepositorySpy.result = true
+    const isValid = await sut.add(mockAddAlunoParams())
+    expect(isValid).toBe(false)
   })
 
   test('Should return an aluno on success', async () => {
