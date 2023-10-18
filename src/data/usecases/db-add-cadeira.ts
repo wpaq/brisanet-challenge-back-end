4fe27fe82ea2439c7d38d037a422134c5e1e0fb2
wpaq <wallysonpabloo@gmail.com>
2023-10-18 17:44:15 -0300

    feat: DbAddCadeira returns false if CheckProfessorByIdRepository returns false

--- src/data/usecases/db-add-cadeira.ts
+++ src/data/usecases/db-add-cadeira.ts
@@ -1,14 +1,19 @@
 import { type AddCadeiraRepository } from '@/data/protocols'
 import { type CadeiraModel } from '@/domain/models/cadeira'
 import { type AddCadeira, type AddCadeiraParams } from '@/domain/usecases/add-cadeira'
+import { type CheckProfessorById } from '@/domain/usecases/check-professor-by-id'
 
 export class DbAddCadeira implements AddCadeira {
   constructor (
-    private readonly addCadeiraRepository: AddCadeiraRepository
+    private readonly addCadeiraRepository: AddCadeiraRepository,
+    private readonly checkProfessorByIdRepository: CheckProfessorById
   ) {}
 
-  async add (data: AddCadeiraParams): Promise<CadeiraModel> {
-    const newCadeira = await this.addCadeiraRepository.add(data)
-    return newCadeira
+  async add (data: AddCadeiraParams): Promise<CadeiraModel | boolean> {
+    const professorExists = await this.checkProfessorByIdRepository.checkById(data.professorId)
+    if (professorExists) {
+      return await this.addCadeiraRepository.add(data)
+    }
+    return false
   }
 }
