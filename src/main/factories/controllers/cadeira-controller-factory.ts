3fc1e9ea10b2a1beb0a881a04d28f2b71bd0db30
wpaq <wallysonpabloo@gmail.com>
2023-10-18 17:47:23 -0300

    feat: add makeDbCheckProfessorById on CadeiraControllerFactory

--- src/main/factories/controllers/cadeira-controller-factory.ts
+++ src/main/factories/controllers/cadeira-controller-factory.ts
@@ -1,10 +1,11 @@
 import { makeCadeiraValidation } from './cadeira-validation-factory'
 import { makeLogControllerDecorator } from '../decorators'
 import { makeDbAddCadeira } from '../usecases/add-cadeira-factory'
+import { makeDbCheckProfessorById } from '../usecases/check-professor-by-id-factory'
 import { CadeiraController } from '@/presentation/controllers'
 import { type Controller } from '@/presentation/protocols'
 
 export const makeCadeiraController = (): Controller => {
-  const controller = new CadeiraController(makeDbAddCadeira(), makeCadeiraValidation())
+  const controller = new CadeiraController(makeDbAddCadeira(), makeCadeiraValidation(), makeDbCheckProfessorById())
   return makeLogControllerDecorator(controller)
 }
