40693330abd4520360042e90d038b6718d028e37
wpaq <wallysonpabloo@gmail.com>
2023-10-18 09:32:56 -0300

    test: ensure DateValidation returns an erro if DateValidator returns false

---
+++ tests/validation/validators/date-validation.spec.ts
@@ -0,0 +1,29 @@
+import { InvalidParamError } from '@/presentation/errors'
+import { DateValidatorSpy } from '@/tests/validation/mocks/mock-date-validation'
+import { DateValidation } from '@/validation/validators'
+
+const field = 'any_field'
+
+type SutTypes = {
+  sut: DateValidation
+  dateValidatorSpy: DateValidatorSpy
+}
+
+const makeSut = (): SutTypes => {
+  const dateValidatorSpy = new DateValidatorSpy()
+  const sut = new DateValidation(field, dateValidatorSpy)
+  return {
+    sut,
+    dateValidatorSpy
+  }
+}
+
+describe('Date Validation', () => {
+  test('Should return an error if DateValidator returns false', () => {
+    const { sut, dateValidatorSpy } = makeSut()
+    dateValidatorSpy.isDateValid = false
+    const date = '10/05/2001'
+    const error = sut.validate({ [field]: date })
+    expect(error).toEqual(new InvalidParamError(field))
+  })
+})
