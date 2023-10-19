b174c73143127d9a4d8428e2684b3c7d054e8ac2
wpaq <wallysonpabloo@gmail.com>
2023-10-19 09:54:21 -0300

    test: ensure CadeirasAlunosController calls AddCadeirasAlunos with correct value

--- tests/presentation/controllers/cadeiras-alunos-controller.spec.ts
+++ tests/presentation/controllers/cadeiras-alunos-controller.spec.ts
@@ -45,4 +45,11 @@ describe('CadeirasAlunos Controller', () => {
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(badRequest(validationSpy.error))
   })
+
+  test('Should call AddCadeirasAlunos with correct value', async () => {
+    const { sut, addCadeirasAlunosSpy } = makeSut()
+    const request = mockRequest()
+    await sut.handle(request)
+    expect(addCadeirasAlunosSpy.addCadeirasAlunosParams).toEqual(request.body)
+  })
 })
