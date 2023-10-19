3774c77efd640f844986d410731660d1345fb20b
wpaq <wallysonpabloo@gmail.com>
2023-10-19 09:50:39 -0300

    test: ensure CadeirasAlunosController return 400 if Validation returns an error

--- tests/presentation/controllers/cadeiras-alunos-controller.spec.ts
+++ tests/presentation/controllers/cadeiras-alunos-controller.spec.ts
@@ -2,6 +2,8 @@ import { AddCadeirasAlunosSpy, ValidationSpy } from '@/tests/presentation/mocks'
 
 import { type HttpRequest } from '@/presentation/protocols'
 import { CadeirasAlunosController } from '@/presentation/controllers'
+import { MissingParamError } from '@/presentation/errors'
+import { badRequest } from '@/presentation/helpers'
 
 import { faker } from '@faker-js/faker'
 
@@ -35,5 +37,12 @@ describe('CadeirasAlunos Controller', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(validationSpy.input).toEqual(request.body)
+  })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.error = new MissingParamError(faker.word.words())
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(badRequest(validationSpy.error))
   })
 })
