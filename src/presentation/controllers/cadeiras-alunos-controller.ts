b5fcc9c1c6d25d600f1ec91d83d611721e63143f
wpaq <wallysonpabloo@gmail.com>
2023-10-19 10:06:12 -0300

    feat: ensure CadeirasAlunosController returns 403 if CheckAlunoById returns false

--- src/presentation/controllers/cadeiras-alunos-controller.ts
+++ src/presentation/controllers/cadeiras-alunos-controller.ts
@@ -1,11 +1,13 @@
 import { type Validation, type Controller, type HttpRequest, type HttpResponse } from '@/presentation/protocols'
-import { badRequest, ok, serverError } from '@/presentation/helpers'
-import { type AddCadeirasAlunos } from '@/domain/usecases'
+import { badRequest, forbidden, ok, serverError } from '@/presentation/helpers'
+import { InvalidParamError } from '@/presentation/errors'
+import { type CheckAlunoById, type AddCadeirasAlunos } from '@/domain/usecases'
 
 export class CadeirasAlunosController implements Controller {
   constructor (
     private readonly addCadeirasAlunos: AddCadeirasAlunos,
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly checkAlunoById: CheckAlunoById
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -16,6 +18,10 @@ export class CadeirasAlunosController implements Controller {
       }
 
       const { alunoId, cadeiraId } = httpRequest.body
+      const alunoExists = await this.checkAlunoById.checkById(alunoId)
+      if (!alunoExists) {
+        return forbidden(new InvalidParamError('alunoId'))
+      }
 
       const cadeirasAlunos = await this.addCadeirasAlunos.add({
         alunoId,
