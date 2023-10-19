9ebb7aa995058eb1a31b6b6eb15ee9e3a4f3c359
wpaq <wallysonpabloo@gmail.com>
2023-10-19 10:11:28 -0300

    feat: ensure CadeirasAlunosController returns 403 if CheckCadeiraById returns false

--- src/presentation/controllers/cadeiras-alunos-controller.ts
+++ src/presentation/controllers/cadeiras-alunos-controller.ts
@@ -1,13 +1,14 @@
 import { type Validation, type Controller, type HttpRequest, type HttpResponse } from '@/presentation/protocols'
 import { badRequest, forbidden, ok, serverError } from '@/presentation/helpers'
 import { InvalidParamError } from '@/presentation/errors'
-import { type CheckAlunoById, type AddCadeirasAlunos } from '@/domain/usecases'
+import { type CheckAlunoById, type AddCadeirasAlunos, type CheckCadeiraById } from '@/domain/usecases'
 
 export class CadeirasAlunosController implements Controller {
   constructor (
     private readonly addCadeirasAlunos: AddCadeirasAlunos,
     private readonly validation: Validation,
-    private readonly checkAlunoById: CheckAlunoById
+    private readonly checkAlunoById: CheckAlunoById,
+    private readonly checkCadeiraById: CheckCadeiraById
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -19,8 +20,12 @@ export class CadeirasAlunosController implements Controller {
 
       const { alunoId, cadeiraId } = httpRequest.body
       const alunoExists = await this.checkAlunoById.checkById(alunoId)
+      const cadeiraExists = await this.checkCadeiraById.checkById(cadeiraId)
       if (!alunoExists) {
         return forbidden(new InvalidParamError('alunoId'))
+      }
+      if (!cadeiraExists) {
+        return forbidden(new InvalidParamError('cadeiraId'))
       }
 
       const cadeirasAlunos = await this.addCadeirasAlunos.add({
