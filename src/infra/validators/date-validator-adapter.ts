b112a32e80cb68f49f13d3022ac575e22f4ff710
wpaq <wallysonpabloo@gmail.com>
2023-10-19 23:04:34 -0300

    feat: add DateValidatorAdapter validate isISO8601

--- src/infra/validators/date-validator-adapter.ts
+++ src/infra/validators/date-validator-adapter.ts
@@ -4,6 +4,6 @@ import validator from 'validator'
 
 export class DateValidatorAdapter implements DateValidator {
   isDate (date: string): boolean {
-    return validator.matches(date, /^\d{2}\/\d{2}\/\d{4}$/)
+    return validator.matches(date, /^(?:\d{4}-\d{2}-\d{2})$/) || validator.isISO8601(date)
   }
 }
