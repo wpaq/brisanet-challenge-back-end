45fcb2b94a0e1c052099c191479b3155ad145218
wpaq <wallysonpabloo@gmail.com>
2023-10-19 22:57:27 -0300

    feat: ensure DbAddCadeira Usecase return false if CheckCadeiraByPeriodRepository returns true

--- src/data/usecases/db-add-cadeira.ts
+++ src/data/usecases/db-add-cadeira.ts
@@ -1,16 +1,18 @@
-import { type AddCadeiraRepository } from '@/data/protocols'
+import { type CheckCadeiraByPeriodRepository, type AddCadeiraRepository } from '@/data/protocols'
 import { type CadeiraModel } from '@/domain/models'
 import { type AddCadeira, type AddCadeiraParams, type CheckProfessorById } from '@/domain/usecases'
 
 export class DbAddCadeira implements AddCadeira {
   constructor (
     private readonly addCadeiraRepository: AddCadeiraRepository,
-    private readonly checkProfessorByIdRepository: CheckProfessorById
+    private readonly checkProfessorByIdRepository: CheckProfessorById,
+    private readonly checkCadeiraByPeriodRepository: CheckCadeiraByPeriodRepository
   ) {}
 
   async add (data: AddCadeiraParams): Promise<CadeiraModel | boolean> {
     const professorExists = await this.checkProfessorByIdRepository.checkById(data.professorId)
-    if (professorExists) {
+    const periodExists = await this.checkCadeiraByPeriodRepository.checkByPeriod(data.dataInicio, data.dataFim)
+    if (professorExists && !periodExists) {
       return await this.addCadeiraRepository.add(data)
     }
     return false
