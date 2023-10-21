b3e04126a0a5b3c0ca79445d58ac893461e2e26f
wpaq <wallysonpabloo@gmail.com>
2023-10-21 15:23:18 -0300

    test: ensure CadeirasAlunosPrismaRepository add an cadeiras alunos on success

---
+++ tests/infra/db/prisma/cadeiras-alunos-prisma-repository.spec.ts
@@ -0,0 +1,33 @@
+import { mockAddAlunoParams, mockAddCadeiraParams, mockAddProfessorParams } from '@/tests/domain'
+
+import { PrismaHelper, CadeirasAlunosPrismaRepository, AlunoPrismaRepository, CadeiraPrismaRepository, ProfessorPrismaRepository } from '@/infra/db/prisma'
+
+describe('CadeirasAlunosPrismaRepository', () => {
+  beforeAll(async () => {
+    await PrismaHelper.connect('test')
+  })
+
+  afterAll(async () => {
+    await PrismaHelper.client.cadeirasAlunos.deleteMany({})
+    await PrismaHelper.client.aluno.deleteMany({})
+    await PrismaHelper.client.cadeira.deleteMany({})
+    await PrismaHelper.disconnect('test')
+  })
+
+  describe('add()', () => {
+    test('Should add an cadeiras alunos on success', async () => {
+      const professor = await new ProfessorPrismaRepository().add(mockAddProfessorParams())
+      const aluno = await new AlunoPrismaRepository().add(mockAddAlunoParams())
+      const cadeira = await new CadeiraPrismaRepository().add(Object.assign({}, mockAddCadeiraParams(), { professorId: professor.id }))
+
+      const sut = new CadeirasAlunosPrismaRepository()
+      await sut.add({
+        alunoId: aluno.id,
+        cadeiraId: cadeira.id
+      })
+
+      const count = await PrismaHelper.client.cadeirasAlunos.count()
+      expect(count).toBe(1)
+    })
+  })
+})
