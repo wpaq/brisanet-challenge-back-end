320702d40958a58c140a03c090d13c2c26a8573d
wpaq <wallysonpabloo@gmail.com>
2023-10-22 13:15:28 -0300

    feat: add config and install dotenv

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,11 +1,11 @@
 import 'module-alias/register'
+import 'dotenv/config'
 
 import app from '@/main/config/app'
-import env from '@/main/config/env'
 import { PrismaHelper } from '@/infra/db/prisma'
 
 PrismaHelper.connect()
   .then(() => {
-    app.listen(5050, () => { console.log(`Server running at http://localhost:${env.port}`) })
+    app.listen(5050, () => { console.log(`Server running at http://localhost:${5050 || process.env.API_PORT}`) })
   })
   .catch(console.error)
