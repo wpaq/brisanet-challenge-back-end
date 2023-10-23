0c81ca411df82a72c279fb8eb7cefb036c606f21
wpaq <wallysonpabloo@gmail.com>
2023-10-23 08:29:31 -0300

    feat: add LoadCadeiraController and route

--- src/main/routes/cadeira-routes.ts
+++ src/main/routes/cadeira-routes.ts
@@ -1,8 +1,10 @@
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
-import { makeCadeiraController } from '@/main/factories/controllers/cadeira-controller-factory'
+import { makeCadeiraController } from '../factories/controllers/cadeira-controller-factory'
+import { makeLoadCadeirasController } from '@/main/factories/controllers/load-cadeiras-controller-factory'
 
 import { type Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/cadeira', adaptRoute(makeCadeiraController()))
+  router.get('/cadeira', adaptRoute(makeLoadCadeirasController()))
 }
