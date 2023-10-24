fe0524b5bc6b150d721339983549ad65c3e0e951
wpaq <wallysonpabloo@gmail.com>
2023-10-24 11:20:46 -0300

    feat: makeDbUpdateCadeirasAlunos inject  makeDbCheckCadeirasAlunosById

--- src/main/factories/usecases/update-cadeiras-alunos-factory.ts
+++ src/main/factories/usecases/update-cadeiras-alunos-factory.ts
@@ -1,8 +1,9 @@
 import { DbUpdateCadeirasAlunos } from '@/data/usecases'
 import { type UpdateCadeirasAlunos } from '@/domain/usecases'
 import { CadeirasAlunosPrismaRepository } from '@/infra/db/prisma'
+import { makeDbCheckCadeirasAlunosById } from './check-cadeiras-alunos-by-id-factory'
 
 export const makeDbUpdateCadeirasAlunos = (): UpdateCadeirasAlunos => {
   const cadeirasAlunosPrismaRepository = new CadeirasAlunosPrismaRepository()
-  return new DbUpdateCadeirasAlunos(cadeirasAlunosPrismaRepository)
+  return new DbUpdateCadeirasAlunos(cadeirasAlunosPrismaRepository, makeDbCheckCadeirasAlunosById())
 }
