a25b61a5b3b81768cabfd8c19c35396f26137ab3
wpaq <wallysonpabloo@gmail.com>
2023-10-23 22:06:26 -0300

    feat: ensure CadeiraPrismaRepository.loadById load an cadeira by id on success

--- src/infra/db/prisma/cadeira-prisma-repository.ts
+++ src/infra/db/prisma/cadeira-prisma-repository.ts
@@ -1,9 +1,9 @@
 import { PrismaHelper } from './helpers/prisma-helper'
-import { type CheckCadeiraByIdRepository, type AddCadeiraRepository, type CheckCadeiraByPeriodRepository, type LoadCadeirasRepository } from '@/data/protocols'
+import { type CheckCadeiraByIdRepository, type AddCadeiraRepository, type CheckCadeiraByPeriodRepository, type LoadCadeirasRepository, type LoadCadeiraByIdRepository } from '@/data/protocols'
 import { type CadeiraModel } from '@/domain/models'
 import { type AddCadeiraParams } from '@/domain/usecases'
 
-export class CadeiraPrismaRepository implements AddCadeiraRepository, CheckCadeiraByIdRepository, CheckCadeiraByPeriodRepository, LoadCadeirasRepository {
+export class CadeiraPrismaRepository implements AddCadeiraRepository, CheckCadeiraByIdRepository, CheckCadeiraByPeriodRepository, LoadCadeirasRepository, LoadCadeiraByIdRepository {
   async add (data: AddCadeiraParams): Promise<CadeiraModel> {
     const newCadeira = await PrismaHelper.client.cadeira.create({
       data: {
@@ -48,5 +48,14 @@ export class CadeiraPrismaRepository implements AddCadeiraRepository, CheckCadei
       include: { cadeirasAlunos: true }
     })
     return cadeiras
+  }
+
+  async loadById (id: string): Promise<CadeiraModel> {
+    const cadeira = await PrismaHelper.client.cadeira.findUnique({
+      where: {
+        id
+      }
+    })
+    return cadeira as CadeiraModel
   }
 }
