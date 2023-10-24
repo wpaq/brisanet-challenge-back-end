f5c25d67d03cdd77c891450e36f6cc55f58630c8
wpaq <wallysonpabloo@gmail.com>
2023-10-24 11:03:32 -0300

    feat: ensure UpdateCadeirasController calls Validation

--- src/presentation/controllers/update-cadeiras-alunos-controller.ts
+++ src/presentation/controllers/update-cadeiras-alunos-controller.ts
@@ -1,16 +1,22 @@
-import { type HttpRequest, type Controller, type HttpResponse } from '@/presentation/protocols'
-import { forbidden, ok, serverError } from '@/presentation/helpers'
+import { type HttpRequest, type Controller, type HttpResponse, type Validation } from '@/presentation/protocols'
+import { badRequest, forbidden, ok, serverError } from '@/presentation/helpers'
 import { InvalidParamError } from '@/presentation/errors'
 import { type CheckCadeirasAlunosById, type UpdateCadeirasAlunos } from '@/domain/usecases'
 
 export class UpdateCadeirasAlunosController implements Controller {
   constructor (
     private readonly updateCadeirasAlunos: UpdateCadeirasAlunos,
-    private readonly checkCadeirasAlunosById: CheckCadeirasAlunosById
+    private readonly checkCadeirasAlunosById: CheckCadeirasAlunosById,
+    private readonly validation: Validation
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+
       const exists = await this.checkCadeirasAlunosById.checkById(httpRequest.body.id)
       if (!exists) {
         return forbidden(new InvalidParamError('cadeirasAlunosId'))
