aecf9b77d4b721644983ef7cbf8121676137782f
wpaq <wallysonpabloo@gmail.com>
2023-10-24 11:00:40 -0300

    feat: ensure UpdateCadeirasAlunosController returns 403 if CheckCadeirasAlunosById returns false

--- src/presentation/controllers/update-cadeiras-alunos-controller.ts
+++ src/presentation/controllers/update-cadeiras-alunos-controller.ts
@@ -1,12 +1,20 @@
 import { type HttpRequest, type Controller, type HttpResponse } from '@/presentation/protocols'
-import { ok, serverError } from '@/presentation/helpers'
-import { type UpdateCadeirasAlunos } from '@/domain/usecases'
+import { forbidden, ok, serverError } from '@/presentation/helpers'
+import { InvalidParamError } from '@/presentation/errors'
+import { type CheckCadeirasAlunosById, type UpdateCadeirasAlunos } from '@/domain/usecases'
 
 export class UpdateCadeirasAlunosController implements Controller {
-  constructor (private readonly updateCadeirasAlunos: UpdateCadeirasAlunos) {}
+  constructor (
+    private readonly updateCadeirasAlunos: UpdateCadeirasAlunos,
+    private readonly checkCadeirasAlunosById: CheckCadeirasAlunosById
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const exists = await this.checkCadeirasAlunosById.checkById(httpRequest.body.id)
+      if (!exists) {
+        return forbidden(new InvalidParamError('cadeirasAlunosId'))
+      }
       const updatedCadeira = await this.updateCadeirasAlunos.update(httpRequest.body)
       return ok(updatedCadeira)
     } catch (error) {
