c338756e87548a7ff0fb6cf77fa8a062ed3abdf4
wpaq <wallysonpabloo@gmail.com>
2023-10-29 19:47:17 -0300

    feat: add LoadAlunoByIdRepository

--- src/data/protocols/aluno/index.ts
+++ src/data/protocols/aluno/index.ts
@@ -1,3 +1,4 @@
 export * from './add-aluno-repository'
 export * from './check-aluno-by-email-repository'
 export * from './check-aluno-by-id-repository'
+export * from './load-aluno-by-id-repository'
