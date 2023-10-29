b3e4f30ce7a850b5cc6b5be157cd73e7adc63a6f
wpaq <wallysonpabloo@gmail.com>
2023-10-29 19:50:29 -0300

    test: ensure DbUpdateCadeirasAlunos calls LoadAlunoByIdRepository with correct id

--- src/data/usecases/cadeiras-alunos/db-update-cadeiras-alunos.ts
+++ src/data/usecases/cadeiras-alunos/db-update-cadeiras-alunos.ts
@@ -1,4 +1,4 @@
-import { type LoadProfessorByIdRepository, type CheckCadeirasAlunosByIdRepository, type UpdateCadeirasAlunosRepository } from '@/data/protocols'
+import { type LoadProfessorByIdRepository, type CheckCadeirasAlunosByIdRepository, type UpdateCadeirasAlunosRepository, type LoadAlunoByIdRepository } from '@/data/protocols'
 import { type CadeirasAlunosModel } from '@/domain/models'
 import { type UpdateCadeirasAlunos, type UpdateCadeirasAlunosParams } from '@/domain/usecases'
 
@@ -6,7 +6,8 @@ export class DbUpdateCadeirasAlunos implements UpdateCadeirasAlunos {
   constructor (
     private readonly updateCadeirasAlunoRepository: UpdateCadeirasAlunosRepository,
     private readonly checkCadeirasAlunosByIdRepository: CheckCadeirasAlunosByIdRepository,
-    private readonly loadProfessorByIdRepository: LoadProfessorByIdRepository
+    private readonly loadProfessorByIdRepository: LoadProfessorByIdRepository,
+    private readonly loadAlunoByIdRepository: LoadAlunoByIdRepository
   ) {}
 
   async update (data: UpdateCadeirasAlunosParams): Promise<CadeirasAlunosModel | boolean> {
@@ -16,6 +17,7 @@ export class DbUpdateCadeirasAlunos implements UpdateCadeirasAlunos {
     if (exists) {
       const cadeirasAlunosUpdated = await this.updateCadeirasAlunoRepository.update(data)
       await this.loadProfessorByIdRepository.loadById(cadeirasAlunosUpdated.professorId)
+      await this.loadAlunoByIdRepository.loadById(cadeirasAlunosUpdated.alunoId)
 
       return cadeirasAlunosUpdated
     }
