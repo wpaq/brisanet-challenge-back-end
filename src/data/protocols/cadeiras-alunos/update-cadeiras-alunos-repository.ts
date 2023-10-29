4a07fd462dcc08f431cf118498dfb76dff9e7f13
wpaq <wallysonpabloo@gmail.com>
2023-10-29 19:37:49 -0300

    feat: ensure DbUpdateCadeirasAlunos calls LoadProfessorByIdRepository with correct id

--- src/data/protocols/cadeiras-alunos/update-cadeiras-alunos-repository.ts
+++ src/data/protocols/cadeiras-alunos/update-cadeiras-alunos-repository.ts
@@ -2,5 +2,5 @@ import { type CadeirasAlunosModel } from '@/domain/models'
 import { type UpdateCadeirasAlunosParams } from '@/domain/usecases'
 
 export interface UpdateCadeirasAlunosRepository {
-  update: (data: UpdateCadeirasAlunosParams) => Promise<CadeirasAlunosModel | boolean>
+  update: (data: UpdateCadeirasAlunosParams) => Promise<CadeirasAlunosModel>
 }
--- src/data/usecases/cadeiras-alunos/db-update-cadeiras-alunos.ts
+++ src/data/usecases/cadeiras-alunos/db-update-cadeiras-alunos.ts
@@ -1,11 +1,12 @@
-import { type CheckCadeirasAlunosByIdRepository, type UpdateCadeirasAlunosRepository } from '@/data/protocols'
+import { type LoadProfessorByIdRepository, type CheckCadeirasAlunosByIdRepository, type UpdateCadeirasAlunosRepository } from '@/data/protocols'
 import { type CadeirasAlunosModel } from '@/domain/models'
 import { type UpdateCadeirasAlunos, type UpdateCadeirasAlunosParams } from '@/domain/usecases'
 
 export class DbUpdateCadeirasAlunos implements UpdateCadeirasAlunos {
   constructor (
     private readonly updateCadeirasAlunoRepository: UpdateCadeirasAlunosRepository,
-    private readonly checkCadeirasAlunosByIdRepository: CheckCadeirasAlunosByIdRepository
+    private readonly checkCadeirasAlunosByIdRepository: CheckCadeirasAlunosByIdRepository,
+    private readonly loadProfessorByIdRepository: LoadProfessorByIdRepository
   ) {}
 
   async update (data: UpdateCadeirasAlunosParams): Promise<CadeirasAlunosModel | boolean> {
@@ -13,7 +14,10 @@ export class DbUpdateCadeirasAlunos implements UpdateCadeirasAlunos {
 
     const exists = await this.checkCadeirasAlunosByIdRepository.checkById(data.id)
     if (exists) {
-      return await this.updateCadeirasAlunoRepository.update(data)
+      const cadeirasAlunosUpdated = await this.updateCadeirasAlunoRepository.update(data)
+      await this.loadProfessorByIdRepository.loadById(cadeirasAlunosUpdated.professorId)
+
+      return cadeirasAlunosUpdated
     }
     return false
   }
