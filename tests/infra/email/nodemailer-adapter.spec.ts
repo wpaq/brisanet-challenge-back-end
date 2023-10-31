c86ee64fc692608a8a60d53d6b6058374803537b
wpaq <wallysonpabloo@gmail.com>
2023-10-30 23:44:07 -0300

    test: ensure NodemailerAdapter calls sendEmail with correct values

---
+++ tests/infra/email/nodemailer-adapter.spec.ts
@@ -0,0 +1,33 @@
+import { NodemailerAdapter } from '@/infra/email'
+import { faker } from '@faker-js/faker'
+
+const to = faker.internet.email()
+const from = faker.internet.email()
+
+const sendMailMock = jest.fn()
+jest.mock('nodemailer', () => ({
+  createTransport: jest.fn().mockImplementation(() => ({
+    sendMail: sendMailMock
+  }))
+}))
+
+const makeSut = (): NodemailerAdapter => {
+  return new NodemailerAdapter()
+}
+
+beforeEach(() => {
+  sendMailMock.mockClear()
+})
+
+describe('NodemailerAdapter', () => {
+  describe('sendMail()', () => {
+    test('Should call sendEmail with correct values', async () => {
+      const sut = makeSut()
+      await sut.send(to, from)
+
+      const sendMailArgs = sendMailMock.mock.calls[0]
+      expect(sendMailArgs[0].to).toBe(to)
+      expect(sendMailArgs[0].from).toBe(from)
+    })
+  })
+})
