54c2102350bb94477a3adb1cb07c9e65a891a827
wpaq <wallysonpabloo@gmail.com>
2023-10-30 23:43:50 -0300

    feat: ensure NodemailerAdapter calls sendEmail with correct values

---
+++ src/infra/email/nodemailer-adapter.ts
@@ -0,0 +1,29 @@
+import { type EmailNotification } from '@/data/protocols'
+
+import nodemailer from 'nodemailer'
+
+const nodemailerConfig = {
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD
+  }
+}
+
+export class NodemailerAdapter implements EmailNotification {
+  async send (to: string, from: string): Promise<boolean> {
+    const transporter = nodemailer.createTransport(nodemailerConfig)
+    const mailOptions = {
+      to,
+      from,
+      subject: 'Email Notification',
+      text: 'Olá, sua matrícula foi aprovada!'
+    }
+    const sending = await transporter.sendMail(mailOptions)
+    if (!sending) {
+      return false
+    }
+    return true
+  }
+}
