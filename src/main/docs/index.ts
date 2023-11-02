b1abfc33b16fb3367d25ade8245241e5cfbd0acf
wpaq <wallysonpabloo@gmail.com>
2023-11-02 19:47:08 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,17 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Back-end challenge Brisalabs',
+    description: 'Essa é a documentação da API Brisalabs',
+    version: '1.0.0',
+    contact: {
+      name: 'Wallyson Pablo',
+      email: 'wallysonpabloo@gmail.com',
+      url: 'https://www.linkedin.com/in/wallyson-pablo'
+    },
+    license: {
+      name: 'ISC',
+      url: 'https://spdx.org/licenses/ISC.html'
+    }
+  }
+}
